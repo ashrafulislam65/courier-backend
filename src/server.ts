168067bfb5195899a20552017810aae76948ed56
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2026-09-06 17:56:29 +0600

    chore: configure Vercel serverless deployment with vercel.json and adapted server entry point

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import app from './app';
 import { env } from './config/env';
 
-const server = app.listen(env.port, () => {
-  console.log(`🚀 Courier API running on port ${env.port} [${env.nodeEnv}]`);
-});
 
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled Rejection:', err);
-  server.close(() => process.exit(1));
-});
+if (env.nodeEnv !== 'production') {
+  app.listen(env.port, () => {
+    console.log(`🚀 Courier API running on port ${env.port} [${env.nodeEnv}]`);
+  });
+}
+
+export default app;
